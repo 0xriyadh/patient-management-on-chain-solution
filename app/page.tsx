bc1b0ab7e0fd4048f8fd9dabd8566f2303c2568e
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-04-06 15:44:47 +0600

    unnecessary files removed
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
-import Test from "./components/Test";
-import GetName from "./components/GetName";
 import { InputForm } from "./components/InputForm";
 
 export default function Home() {
   return (
       <main className="container mx-auto">
-          {/* <GetName /> */}
           <InputForm />
-          {/* <Test /> */}
       </main>
   );
 }
