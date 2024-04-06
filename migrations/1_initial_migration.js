f348f5989cdfb90e07e7079ac1eaeb780519b926
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-04-06 15:43:24 +0600

    unnecessary files removed
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,5 @@
-var MyName = artifacts.require("./MyName.sol");
 var PatientManagement = artifacts.require("./PatientManagement.sol");
 
 module.exports = function (deployer) {
-    deployer.deploy(MyName);
     deployer.deploy(PatientManagement);
 };
